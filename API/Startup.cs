4c2d31d99d831afbff0ccb1c6ce6265a18aae673
SELISE\abu.adnan <abu.adnan@selise.ch>
2022-08-24 12:54:30 +0600

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,7 @@ namespace API
                 option.UseSqlite(_configuration.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +44,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
